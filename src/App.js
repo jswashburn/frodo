11a166c374cb337bdb4cd8bb44a7c62a846aebbe
Jeffrey Washburn <jwashburn@microsoft.com>
2022-02-15 09:39:29 -0800

    update content

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Ethan and Conrad. 🔥
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cat pictures
         </a>
       </header>
     </div>
