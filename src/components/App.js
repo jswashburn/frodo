116796a4e05b7dd246e2a29c2adfb40752d3096d
Jeffrey Washburn <jeffrey.s.washburn@outlook.com>
2022-05-28 11:26:41 -0700

    refactor main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,68 @@
 import React, { useState } from "react";
 import "../styles/App.scss";
 
-import { fetchQuotes, fetchAllCharacters } from "../utils/lotrClient.js";
+import { getAllDocs } from "../utils/lotrClient.js";
 import { QuoteDisplay } from "./QuoteDisplay";
 
+function PickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 export default function App() {
-  const [quoteDisplay, setQuoteDisplay] = useState({
-    name: null,
-    dialogText: null,
+  const [state, setState] = useState({
+    quoteDisplay: <QuoteDisplay name={null} dialogText={null} />,
+    allCharacters: [],
+    allQuotes: [],
   });
-  const [characters, setCharacters] = useState([]);
-  const [quotes, setQuotes] = useState([]);
-  const [ready, setReady] = useState(false);
-
-  const updateQuoteDisplay = (quoteArray, characterArray) => {
-    // grab random quote
-    const randomlyPickedQuote =
-      quoteArray[Math.floor(Math.random() * quoteArray.length)];
-
-    // find characters name that said the quote
-    const found = characterArray.filter(
-      (character) => character._id === randomlyPickedQuote.character
-    );
-    const characterName = found[0]?.name ?? "Unknown Character";
-
-    setQuoteDisplay({
-      name: characterName,
-      dialogText: randomlyPickedQuote.dialog,
-    });
-  };
-
-  const renderQuoteDisplay = () => {
-    if (!ready) return (
-      <p>Loading...</p>
-    );
-
-    return (
-      <>
-        <QuoteDisplay
-          name={quoteDisplay.name}
-          dialogText={quoteDisplay.dialogText}
-        />
-        <p className="DisableTextSelection">Click anywhere for next quote...</p>
-      </>
-    );
-  }
 
   React.useEffect(() => {
     const init = async () => {
-      const fetchedQuotes = await fetchQuotes();
-      const fetchedCharacters = await fetchAllCharacters();
+      const fetchedQuotes = await getAllDocs("quote");
+      const fetchedCharacters = await getAllDocs("character");
 
-      setQuotes(fetchedQuotes);
-      setCharacters(fetchedCharacters);
-      updateQuoteDisplay(fetchedQuotes, fetchedCharacters);
-
-      setReady(true);
+      setState({
+        quoteDisplay: RenderRandomQuoteDisplay(
+          fetchedQuotes,
+          fetchedCharacters
+        ),
+        allCharacters: fetchedCharacters,
+        allQuotes: fetchedQuotes,
+      });
     };
     init();
   }, []);
 
+  function RenderRandomQuoteDisplay(quotes, characters) {
+    const randomlyPickedQuote = PickRandom(quotes);
+    const character = characters.filter(
+      (c) => c._id === randomlyPickedQuote.character
+    );
+    const characterName = character[0]?.name ?? "Unknown Character";
+
+    return (
+      <QuoteDisplay
+        name={characterName}
+        dialogText={randomlyPickedQuote.dialog}
+      />
+    );
+  }
+
   return (
-    <div className="App" onClick={() => updateQuoteDisplay(quotes, characters)}>
-      {renderQuoteDisplay()}
+    <div
+      className="App DisableTextSelection"
+      onClick={() =>
+        setState((prev) => ({
+          ...prev,
+          quoteDisplay: RenderRandomQuoteDisplay(
+            prev.allQuotes,
+            prev.allCharacters
+          ),
+        }))
+      }
+    >
+      {state.quoteDisplay}
+      <p>Click anywhere for next quote...</p>
     </div>
   );
 }
+
